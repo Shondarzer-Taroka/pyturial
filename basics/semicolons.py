5d098b127d9c90b7ef7f317739b06e6821c7be30
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2026-03-16 14:22:30 +0600

    semicolons in python optional

---
+++ basics/semicolons.py
@@ -0,0 +1,5 @@
+# Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read:
+
+
+x=10; y=12; age=23; name='Fawza'
+print(name,age)
