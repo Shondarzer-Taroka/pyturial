7c9e3490603d6b2c7cc60bf859108a8cd107dcb8
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2026-03-25 12:50:43 +0600

    global and function variable and how to function variable to global variable

---
+++ basics/global-variable.py
@@ -0,0 +1,35 @@
+
+# x=90
+
+# def balkl():
+#          print(x)
+
+# balkl()
+
+# x = "awesome"
+
+# def myfunc():
+#   print("Python is " + x)
+
+# myfunc()
+
+
+
+x='Yasmine'
+
+
+def myFunc():
+    x=90
+    print(x)
+myFunc()
+
+
+def wear():
+    global name 
+    name= "Galib"
+
+    print(name,'function in')
+
+wear()
+
+print(name,'out ')
